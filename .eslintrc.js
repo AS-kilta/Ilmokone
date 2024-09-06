aefcafc98a81b3627685100f44abeeb9e08f9523
Akseli Pihlajamaa <akselijuhanipihlajamaa@gmail.com>
2024-09-06 19:18:34 +0300

    try fixing issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,50 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: [
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "project": [
       "packages/ilmomasiina-models/tsconfig.json",
       "packages/ilmomasiina-components/tsconfig.json",
       "packages/ilmomasiina-frontend/tsconfig.json",
-      "packages/ilmomasiina-backend/tsconfig.json",
+      "packages/ilmomasiina-backend/tsconfig.json"
     ],
-    tsconfigRootDir: __dirname,
+    "tsconfigRootDir": __dirname,
     // https://github.com/typescript-eslint/typescript-eslint/issues/2094
-    EXPERIMENTAL_useSourceOfProjectReferenceRedirect: true,
+    "EXPERIMENTAL_useSourceOfProjectReferenceRedirect": true
   },
-  ignorePatterns: [
+  "ignorePatterns": [
     "**/node_modules/**",
     "**/dist/**",
     "**/build/**",
     ".eslintrc.js",
     "jest.config.js",
     "*.scss",
-    "*.json",
+    "*.json"
   ],
-  settings: {
-    react: {
-      pragma: "React",
-      version: "16.12",
+  "settings": {
+    "react": {
+      "pragma": "React",
+      "version": "16.12"
     },
   },
-  extends: ["airbnb", "airbnb/hooks", "airbnb-typescript", "prettier"],
-  plugins: ["@typescript-eslint", "promise", "simple-import-sort", "jest"],
-  env: {
-    browser: true,
+  "extends": [
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "prettier"
+  ],
+  "plugins": [
+    "@typescript-eslint",
+    "promise",
+    "simple-import-sort",
+    "jest"
+  ],
+  "env": {
+    "browser": true
   },
-  rules: {
+  "rules": {
+    "max-len": ["error", 120, 2],
+    "@typescript-eslint/semi": ["error", "always"],
+    "@typescript-eslint/quotes": ["error", "single"],
     // To allow grouping of class members - especially for Models.
     "@typescript-eslint/lines-between-class-members": "off",
     // Doesn't increase code quality with redux.
@@ -39,7 +52,7 @@ module.exports = {
     // Allow i++ in for loops.
     "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
     // We are targeting ES5 or higher.
-    radix: ["error", "as-needed"],
+    "radix": ["error", "as-needed"],
     // ...I know what I'm doing.
     "no-control-regex": "off",
     // Not usable with formik.
@@ -51,33 +64,29 @@ module.exports = {
     // the problem at all, and useCallback is just plain ugly.
     "react/jsx-no-bind": "off",
     // Add any custom hooks here
-    "react-hooks/exhaustive-deps": [
-      "error",
-      {
-        additionalHooks: "useAbortableEffect|useAbortablePromise",
-      },
-    ],
+    "react-hooks/exhaustive-deps": ["error", {
+      additionalHooks: "useAbortableEffect|useAbortablePromise",
+    }],
     // Prefer arrow functions to functions expressions, as that's what was done
     // when this rule was introduced.
-    "react/function-component-definition": [
-      "error",
-      {
-        namedComponents: ["function-declaration", "arrow-function"],
-        unnamedComponents: "arrow-function",
-      },
-    ],
+    "react/function-component-definition": ["error", {
+      namedComponents: ["function-declaration", "arrow-function"],
+      unnamedComponents: "arrow-function",
+    }],
     // Allow dev deps in test files.
-    "import/no-extraneous-dependencies": [
-      "error",
-      {
-        devDependencies: ["**/test/**", "**/vite.config.ts", "**/vitest.config.ts", "**/.eslintrc.js"],
-      },
-    ],
+    "import/no-extraneous-dependencies": ["error", {
+      devDependencies: [
+        "**/test/**",
+        "**/vite.config.ts",
+        "**/vitest.config.ts",
+        "**/.eslintrc.js"
+      ],
+    }],
     // Sort imports: React first, then npm packages, then local files, then CSS.
     "simple-import-sort/imports": [
       "error",
       {
-        groups: [
+        "groups": [
           ["^react$"],
           ["^@?\\w"],
           // Anything that does not start with a dot.
@@ -85,23 +94,20 @@ module.exports = {
           // Anything that starts with a dot, or is from one of our packages.
           ["^@tietokilta/", "^"],
           // Css
-          ["css$"],
-        ],
-      },
+          ["css$"]
+        ]
+      }
     ],
     // Prevent imports from "src/...". VS Code adds these automatically, but they
     // break when compiled.
     "no-restricted-imports": [
       "error",
       {
-        patterns: [
-          {
-            group: ["src/*"],
-            message:
-              'This import will break when compiled by tsc. Use a relative path instead, or "../src/" in test files.',
-          },
-        ],
+        "patterns": [{
+          group: ["src/*"],
+          message: "This import will break when compiled by tsc. Use a relative path instead, or \"../src/\" in test files."
+        }],
       },
     ],
-  },
+  }
 };
